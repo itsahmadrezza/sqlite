a160606321a78ad94d19f808b84ee1f81d2878bb
itsahmadrezza <99419920+itsahmadrezza@users.noreply.github.com>
2022-06-12 08:41:37 -0700

    add user editor

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -14,7 +14,19 @@ def index(request):
 def flight(request, flight_id):
     flight = Flight.objects.get(id = flight_id)
     passengers = flight.passengers.all()
+    non_passengers = Passenger.objects.exclude(flights = flight).all()
     return render(request, 'flights/flight.html',{
         "flight": flight,
-        "passenger": passengers
+        "passengers": passengers,
+        "non_passengers":non_passengers
     })
+
+
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(id = flight_id)   # pk == primary key
+        passenger_id = int(request.POST['passenger'])
+        passenger = Passenger.objects.get(id = passenger_id)
+
+
